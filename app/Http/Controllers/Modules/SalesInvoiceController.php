aa0b68eb50c2669199211d739d51afb9c9610d87
Bermar Balibalos <bermar@avail.at>
2018-08-11 10:09:38 +0800

    Routing

---
+++ app/Http/Controllers/Modules/SalesInvoiceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Modules;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SalesInvoiceController extends Controller
+{
+    public function index()
+    {
+        return 'salesinvoice index';
+    }
+}
