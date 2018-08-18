3ce079b24baf6fd9039d8ddec70c7a4326ff307f
Bermar Balibalos <bermar@avail.at>
2018-08-18 16:16:50 +0800

    Add api get current user

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,11 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1'], function (){
     Route::post('login', 'Auth\LoginController@loginViaJwt');
+
+    Route::group(['middleware' => 'jwt.auth'], function (){
+        Route::get('current-user', function(){
+            return Tymon\JWTAuth\Facades\JWTAuth::user();
+        });
+    });
+    
 });
